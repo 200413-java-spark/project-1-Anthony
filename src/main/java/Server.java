e596adbb6d2f27eb0e1cb7d16b46cdf866b4c004
winkie224 <leanthony104@yahoo.com>
2020-05-14 20:20:29 -0500

    server

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,11 +2,17 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import io.SQLIo;
+import io.parseCSVFile;
+import io.netty.util.internal.SystemPropertyUtil;
 import items.UniqueItem;
 import servlet.testServlet;
 
 import java.io.File;
 import java.sql.Array;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
@@ -21,27 +27,32 @@ public class Server {
         tomcat.addServlet("/spark", "testServlet", new testServlet()).addMapping("/testServlet");
     }
 
-    public static void main(String args[]) {
-        SparkConf conf = new SparkConf().setMaster("local").setAppName("Project 1 Anthony");
-        JavaSparkContext sparkContext = new JavaSparkContext(conf);
-        JavaRDD<String> rows = sparkContext.textFile("src/main/resources/TestData.csv").cache();
-        // Function<String, Boolean> filter = k -> (k.matches("Unique+"));
-        JavaRDD<String> itemRowsOnly = rows.filter(r -> {
-            return r.matches(".*Unique.*");
-        }).cache();
+    public static void main(String args[]) throws SQLException {
+        // System.out.println("args: " + args[0]);
+        if (args[0].equals("load")) {
+            SparkConf conf = new SparkConf().setMaster("local").setAppName("Project 1 Anthony");
+            JavaSparkContext sparkContext = new JavaSparkContext(conf);
+            JavaRDD<UniqueItem> itemList = new parseCSVFile().createItemRDD(sparkContext);
+            System.out.println("itemlist: " + itemList.count());
+            // List<UniqueItem> arrayList = parseCSVFile.createListFromRDD(itemList);
+            itemList.foreach(f -> {
+                System.out.println(f.getName());
+                SQLIo.insertSQL(f);
+            });
 
-        // System.out.println(itemRowsOnly.collect());
-        for (String line : itemRowsOnly.collect()) {
-            System.out.println(line + "3");
+            sparkContext.close();
+            // execTomcat();
+
+            // Runtime.getRuntime().addShutdownHook(new Thread() {
+            // @Override
+            // public void run() {
+            // sparkContext.close();
+            // }
+            // });
+        } else {
+            System.out.println("not loaded" + args[0]);
+            // execTomcat();
         }
-        sparkContext.close();
-        // execTomcat();
 
-        // Runtime.getRuntime().addShutdownHook(new Thread() {
-        // @Override
-        // public void run() {
-        // sparkContext.close();
-        // }
-        // });
     }
 }
