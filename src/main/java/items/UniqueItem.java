4c3719129aa5de894f4042c45f5adadf54f925fb
winkie224 <leanthony104@yahoo.com>
2020-05-14 20:19:40 -0500

    uu

--- src/main/java/items/UniqueItem.java
+++ src/main/java/items/UniqueItem.java
@@ -1,15 +1,47 @@
 package items;
 
-import java.sql.Date;
+import java.io.Serializable;
 
 public class UniqueItem extends Item {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     public String type;
     public String baseType;
     public int links;
 
-    public UniqueItem(String league, Date date, int itemID, String type, String name, String baseType, double value) {
+    public UniqueItem(String league, String date, int itemID, String type, String name, String baseType, double value) {
         super(league, date, itemID, name, value);
         this.type = type;
         this.baseType = baseType;
+    }
+
+    public String getLeague() {
+        return this.league;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public int getItemID() {
+        return this.itemID;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBaseType() {
+        return this.baseType;
+    }
+
+    public double getValue() {
+        return this.value;
     }
 }
